8ac06626f285ba3899b3d527b9e8a209d29fa272
Christian Echtermeyer <chrech.birr@gmail.com>
2018-03-24 00:04:52 +0100

    adding basic shooting ability

---
+++ Game/Assets/Scripts/SpellScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellScript : MonoBehaviour
+{
+
+    public Vector3 destination;
+    private Vector3 direction;
+
+    private float spellSpeed = 0.3f;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+        // TODO
+	    transform.Translate(destination * spellSpeed);
+	}
+}
